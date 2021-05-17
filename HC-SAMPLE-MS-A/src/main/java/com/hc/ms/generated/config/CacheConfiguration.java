f67203475510fc4911d2b11d2c0ed5aace486853
Damien Ji ZiYang <ziyang.ji@ncsi.com.cn>
2021-05-17 16:11:38 +0800

    modify the instance id

--- HC-SAMPLE-MS-A/src/main/java/com/hc/ms/generated/config/CacheConfiguration.java
+++ HC-SAMPLE-MS-A/src/main/java/com/hc/ms/generated/config/CacheConfiguration.java
@@ -71,13 +71,13 @@ public class CacheConfiguration {
     @Bean
     public HazelcastInstance hazelcastInstance(JHipsterProperties jHipsterProperties) {
         log.debug("Configuring Hazelcast");
-        HazelcastInstance hazelCastInstance = Hazelcast.getHazelcastInstanceByName("HC-SAMPLE-MS-A");
+        HazelcastInstance hazelCastInstance = Hazelcast.getHazelcastInstanceByName("HC-SAMPLE-MS-B");
         if (hazelCastInstance != null) {
             log.debug("Hazelcast already initialized");
             return hazelCastInstance;
         }
         Config config = new Config();
-        config.setInstanceName("HC-SAMPLE-MS-A");
+        config.setInstanceName("HC-SAMPLE-MS-B");
         config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
         if (this.registration == null) {
             log.warn("No discovery service is set up, Hazelcast cannot create a cluster.");
