113a0891addb63e86592c9b38c080d4406a5a7f6
Damien Ji ZiYang <ziyang.ji@ncsi.com.cn>
2021-05-20 16:04:55 +0800

    delete center

--- HC-MANAGEMENT-CENTER/mc-conf.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-
-java -cp hazelcast-mancenter-3.12.9.war com.hazelcast.webmonitor.cli.MCConfCommandLine %*
--- HC-MANAGEMENT-CENTER/mc-conf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -cp hazelcast-mancenter-3.12.9.war com.hazelcast.webmonitor.cli.MCConfCommandLine "$@"
